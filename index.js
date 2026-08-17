a065c6dd3465603b1ca2db671745d10815cdfcff
JT <74469317+JaTraumDev@users.noreply.github.com>
2026-08-17 17:05:48 +0200

    feat: add functionality to the index.js
    you can make books, add books to a library and show all books

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const myLibrary = [];
 
-function book() {
-
+function book(name, author) {
+	this.name = name
+	this.author = author
+	this.id = crypto.randomUUID();
+	this.displayInfo = function () {
+		console.log(`Name: ${this.name}`)
+		console.log(`Author: ${this.author}`)
+		console.log(`id: ${this.id}`)
+	}
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(name, author) {
+	const bookObject = new book(name, author);
+	myLibrary.push(bookObject);
 }
+
+function showBooks() {
+	myLibrary.forEach((bookObject) => {
+		bookObject.displayInfo();
+		console.log("---")
+	})
+}
+
+
+addBookToLibrary("Dynamite", "James Sunderland");
+addBookToLibrary("I Have No Mouth, and I Must Scream", "Harlan Ellison");
+addBookToLibrary("Fifty Shades of Grey", "E.L. James");
+showBooks();
