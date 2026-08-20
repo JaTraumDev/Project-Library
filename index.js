3d517ede8afa44cb288a66c063174e496caf7340
JT <74469317+JaTraumDev@users.noreply.github.com>
2026-08-20 15:42:27 +0200

    feat: add bookColors as an array

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const myLibrary = [];
 
+const bookColors = [
+	"#dc8a78",
+	"#dd7878",
+	"#ea76cb",
+	"#8839ef",
+	"#d20f39",
+	"#e64553",
+	"#fe640b",
+	"#df8e1d",
+	"#40a02b",
+	"#179299",
+	"#04a5e5",
+	"#209fb5",
+	"#1e66f5",
+	"#7287fd",
+];
+
 const bookshelf = document.getElementById("bookshelf");
 
 function book(name, author) {
